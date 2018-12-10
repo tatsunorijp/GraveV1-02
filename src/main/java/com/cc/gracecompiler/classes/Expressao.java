e37208e94fcba542be88c84f1ade6c632affb1c1
Tatsunori <tatsunoripai@gmail.com>
2018-12-10 17:33:14 -0200

    trabalho maldito

--- src/main/java/com/cc/gracecompiler/classes/Expressao.java
+++ src/main/java/com/cc/gracecompiler/classes/Expressao.java
@@ -7,6 +7,21 @@ import java.util.List;
 
 public class Expressao {
 
+    /*public Tipo tipo = null;
+    public Operador op = null;
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public Operador getOp() {
+        return op;
+    }*/
+
+
+
+
+
     public Tipo tipo;
     public List<Token> tokenList = new ArrayList<Token>();
     public Operador op = null;
@@ -15,4 +30,30 @@ public class Expressao {
     public Tipo getTipo() {
         return tipo;
     }
+
+    public void analiseSemantica(){
+
+        if(op == Operador.AND || op == Operador.OR || op == Operador.NEGATION){
+            for(Token t: tokenList){
+                if(t.kind != Tipo.BOOL.kind) throw new RuntimeException("ERRO: Tipos incompativeis na expressao. \r\nLine: " + t.beginLine);
+            }
+        }
+
+        if(op == Operador.BIGGER || op == Operador.BIGGEREQUAL || op == Operador.SMALLER || op == Operador.SMALLEREQUAL){
+            if(tokenList.get(0).kind != Tipo.INT.kind || tokenList.get(1).kind != Tipo.INT.kind) throw new RuntimeException("ERRO: Tipos incompativeis na expressao. \r\nLine: " + tokenList.get(0).beginLine);
+        }
+
+        if(op == Operador.EQUALS || op == Operador.DIFFERENT){
+            if(tokenList.get(0).kind != tokenList.get(1).kind) throw new RuntimeException("ERRO: Tipos incompativeis na expressao. \r\nLine: " + tokenList.get(0).beginLine);
+        }
+
+        if(op == Operador.PLUS || op == Operador.MINUS || op == Operador.TIMES || op == Operador.DIVISION || op == Operador.PERCENT){
+            for(Token t: tokenList){
+                if(t.kind != Tipo.INT.kind) throw new RuntimeException("ERRO: Tipos incompativeis na expressao. \r\nLine: " + t.beginLine);
+            }
+        }
+
+
+    }
+
 }
--- src/main/java/com/cc/gracecompiler/classes/Tipo.java
+++ src/main/java/com/cc/gracecompiler/classes/Tipo.java
@@ -1,10 +1,18 @@
 package com.cc.gracecompiler.classes;
 
 public enum Tipo {
-    INT(47), BOOL(48),STRING(49), ID(50), VOID(15);
+    INT(47), BOOL(48),WORD(49), ID(50), VOID(15);
     public final int kind;
 
     Tipo(int kind){
         this.kind = kind;
     }
+
+    public static Tipo getTipo(int kind) {
+        for (Tipo t : Tipo.values()) {
+            if (t.kind == kind) return t;
+        }
+        throw new IllegalArgumentException("Tipo nao encontrado");
+    }
+
 }
